aee989917fadd4d3c7ccabed7d50db94a26f8482
newlix <newlix@me.com>
2023-11-30 20:14:33 +0800

    pico

--- Makefile
+++ Makefile
@@ -1,11 +1,7 @@
-css:
-	npx tailwindcss -i styles/styles.css -o static/styles/styles.css
-.PHONY: css
-
 dev:
 	zola serve
 .PHONY: dev
 
 prod:
 	zola build
-.PHONY: 
+.PHONY:
--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: ["./templates/**/*.html", "./theme/**/*.html",  "./themes/"],
-  theme: {},
-  variants: {},
-  plugins: [
-      require('@tailwindcss/typography'),
-  ],
-};
